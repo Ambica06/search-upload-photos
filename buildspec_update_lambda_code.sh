7f792c80b0ad2063b96c8534d7e2d8782eda3ba7
Ambica06 <bhatia.ambica@gmail.com>
2025-04-29 04:04:43 -0400

    test

--- buildspec_update_lambda_code.sh
+++ buildspec_update_lambda_code.sh
@@ -7,13 +7,13 @@ echo "Updating both Lambda functions..."
 # Update index-photos
 echo "Updating index-photos Lambda..."
 aws lambda update-function-code \
-  --function-name lambda_function \
+  --function-name index-photos \
   --zip-file fileb://index-photos.zip
 
 # Update search-photos
 echo "Updating search-photos Lambda..."
 aws lambda update-function-code \
-  --function-name lambda_function \
+  --function-name search-photos \
   --zip-file fileb://search-photos.zip
 
 echo "Both Lambda functions updated successfully!"
